da8f616abc1549cfb74882794c1826bde149c3bf
IgorIoT13 <ihor.slodziak.ir.2022@lpnu.ua>
2023-10-26 12:16:19 +0300

    add content

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import './App.css';
 import Navpan from './components/nav/Navpan'
 import Contend from "./components/header/Contend";
+import Header from "./components/container/Header";
 
 
 function App() {
   return (
     <div className="App">
         <Navpan />
+        <Header />
         <Contend />
     </div>
   );
