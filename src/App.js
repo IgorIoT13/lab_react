854a5fd899919c8a23a8b51da0d12fe80e543bd1
IgorIoT13 <ihor.slodziak.ir.2022@lpnu.ua>
2023-10-26 08:36:13 +0300

    add navpan

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './App.css';
+import Navpan from './components/nav/Navpan'
+
+
+function App() {
+  return (
+    <div className="App">
+        <Navpan />
+    </div>
+  );
+}
+
+export default App;
